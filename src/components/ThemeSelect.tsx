ad48382f22ccc50b41262615f950cc99f892511a
Kelsey Huse <kelseyhuse30@gmail.com>
2025-03-08 16:35:03 -0600

    use Chakra For component

--- src/components/ThemeSelect.tsx
+++ src/components/ThemeSelect.tsx
@@ -1,4 +1,4 @@
-import { Tabs } from "@chakra-ui/react"
+import { For, Tabs } from "@chakra-ui/react"
 import { THEMES } from "../constants";
 
 interface ThemeHandler {
@@ -10,11 +10,13 @@ export function ThemeSelect({ currentTheme, onThemeClick }: ThemeHandler) {
     return (
         <Tabs.Root value={currentTheme} variant="plain" onValueChange={(e) => onThemeClick(e.value)} size="lg">
             <Tabs.List bg="bg.muted" rounded="l3" p="1">
-                {THEMES.map((theme) => (
+                <For each={THEMES}>
+                {(theme) => (
                     <Tabs.Trigger value={theme}>
                         {theme.toUpperCase()}
                     </Tabs.Trigger>
-                ))}
+                )}
+                </For>
                 <Tabs.Indicator rounded="l2" />
             </Tabs.List>
         </Tabs.Root>
