64866a1990649b58075daf6bee87d7e7812bd01b
Kelsey Huse <kelseyhuse30@gmail.com>
2025-07-14 16:22:26 -0500

    login flow

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,10 +1,16 @@
-import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "@chakra-ui/react";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <Button
+    size="xl"
+    backgroundColor={"#60c9b7"}
+    width={250}
+    onClick={() => loginWithRedirect()}>
+    Log In
+  </Button>;
 };
 
 export default LoginButton;
