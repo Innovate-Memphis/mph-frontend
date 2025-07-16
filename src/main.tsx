8c4aee37a7921bf7ebff7889611616a933ea9734
Kelsey Huse <kelseyhuse30@gmail.com>
2025-07-16 15:36:41 -0500

    try this...

--- src/main.tsx
+++ src/main.tsx
@@ -19,6 +19,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
             domain={import.meta.env.VITE_AUTH0_DOMAIN}
             clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
             authorizationParams={{ redirect_uri: window.location.origin }}
+            useCookiesForTransactions={true}
           >
             <App />
           </Auth0Provider>
