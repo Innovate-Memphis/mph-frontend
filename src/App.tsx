d421cc1fee4e59a4d568be72a8dbf0ac59b9bb3e
Kelsey Huse <kelseyhuse30@gmail.com>
2025-11-23 14:22:44 -0600

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -70,6 +70,8 @@ export default function Page() {
 
   const { isLoading, isAuthenticated, error, user } = useAuth0();
 
+  console.log(JSON.stringify(user));
+
   const queryString = window.location.search;
 
   if (isLoading) {
