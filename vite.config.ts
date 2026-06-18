ff4ad6b206a3e7bd162b06cab7f50be79dfeff5c
Kelsey Huse <kelseyhuse30@gmail.com>
2026-06-18 17:30:37 -0500

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/' : '/dev/',
   plugins: [react()],
-});
+}));
