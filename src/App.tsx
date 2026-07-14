fe0a93a652c859b6a0a1c7e12b8e6617155cbf41
Kelsey Huse <kelseyhuse30@gmail.com>
2026-07-14 15:41:55 -0500

    refactor into FilterPane component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,8 @@ import {
   Button,
   Center,
   Flex,
-  Grid,
-  GridItem,
   HStack,
   Link,
-  StackSeparator,
   Spinner,
   Stack,
   Text,
@@ -15,21 +12,16 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import { LuRotateCcw } from "react-icons/lu";
-import { Filters } from "@feltmaps/js-sdk";
 import Joyride, { ACTIONS, EVENTS, CallBackProps } from 'react-joyride';
 import { useAuth0 } from '@auth0/auth0-react';
 import LogRocket from 'logrocket';
 
 import { ThemeSelect } from "./components/ThemeSelect";
-import { FilterSelection } from "./components/FilterSelection";
 import { FilterSwitch } from "./components/FilterSwitch";
 import { AggregationsSwitch } from "./components/AggregationsSwitch";
-import { DateRangeSlider } from "./components/DateRangeSlider";
-import { LivingUnitsCategorySelect } from "./components/LivingUnitsCategorySelect";
-import { LandUseCategorySelect } from "./components/LandUseCategorySelect";
-import { GeographicFiltersSelect } from "./components/GeographicFiltersSelect";
 import { HelpMenu } from "./components/HelpMenu";
 import { MPHLogo } from "./components/MPHLogo";
+import { FilterPane } from "./components/FilterPane";
 
 // @ts-ignore
 import { AlertMessage } from "./components/AlertMessage";
@@ -42,31 +34,20 @@ import RequestAccessButton from "./components/RequestAccessButton";
 
 import { useFeltEmbed } from "./feltUtils";
 import {
-  DEFAULT_BUILT_YEAR_FILTERS,
   EXPLORE,
   FAQ_LINK,
   FAQ_LINK_TEXT,
   FELT_MAP_ID,
   FILTERED_PARCEL_LAYER_ID,
-  FILTERS_TO_FELT_FILTER,
-  GEOGRAPHIC_FELT_FILTER_MAP,
   GROUP_LAYERS_TO_HIDE,
-  LIVING_UNITS_CATEGORY_FILTER,
-  LUC_ZONING_FELT_FILTER_MAP,
   LAYERS_TO_HIDE,
   LOGIN_FAILURE_MESSAGE,
-  MIN_YEAR_BUILT_FILTER,
-  MAX_YEAR_BUILT_FILTER,
-  MFH_HOMES,
-  SFH_HOMES,
-  SFH_MFH_HOMES,
   THEME_TO_GROUP_LAYER_MAP,
   THEME_TO_PARCEL_LAYER_MAP,
   THEMES,
   THEMES_WITHOUT_AGGREGATIONS,
   TOUR_STEPS,
 } from "./constants";
-import { filterUtils } from "./utils";
 import { useState, useEffect } from "react";
 
 export default function Page() {
@@ -134,17 +115,11 @@ export default function Page() {
   const [showFilters, setShowFilters] = useState(true);
   const [showAggregations, setShowAggregations] = useState(false);
   const [currentTheme, setCurrentTheme] = useState(EXPLORE);
-  const [currentFilters, setCurrentFilters] = useState([]);
-  const [currentFilterBuildDate, setCurrentFilterBuildDate] = useState(DEFAULT_BUILT_YEAR_FILTERS);
-  const [currentFilterLivingUnitsCategory, setCurrentFilterLivingUnitsCategory] = useState([]);
-  const [currentFilterLandUseCategory, setCurrentFilterLandUseCategory] = useState([]);
-  const [currentLandUseZoningFilter, setcurrentLandUseZoningFilter] = useState([]);
-  const [currentLandUseZoningValues, setcurrentLandUseZoningValues] = useState([]);
-  const [currentGeographicFilter, setCurrentGeographicFilter] = useState([]);
-  const [currentGeoFilteredValues, setCurrentGeoFilteredValues] = useState([]);
   const [dataYear, setDataYear] = useState<null | number>(null);
   const [run, setRun] = useState(false);
   const [stepIndex, setStepIndex] = useState(0);
+  // changing the key prop causes the component to re-render. This is used to reset the filters.
+  const [filterKey, setFilterKey] = useState(0);
 
   useEffect(() => {
     const getMaxYearData = async () => {
@@ -222,68 +197,6 @@ export default function Page() {
   }, [felt, currentTheme, showAggregations]);
 
   useEffect(() => {
-    const updateLayerFilter = async () => {
-      if (felt) {
-
-        const allLayerFilters = new Map(FILTERS_TO_FELT_FILTER);
-        const allFeltFormattedFilters: Filters[] = currentFilters.map((f) => allLayerFilters.get(f) || null);
-
-        if (currentFilterBuildDate[0] !== DEFAULT_BUILT_YEAR_FILTERS[0]) {
-          let minYearBuiltFilter = MIN_YEAR_BUILT_FILTER;
-          // @ts-ignore
-          minYearBuiltFilter[2] = currentFilterBuildDate[0]
-          allFeltFormattedFilters.push(minYearBuiltFilter)
-        }
-
-        if (currentFilterBuildDate[1] !== DEFAULT_BUILT_YEAR_FILTERS[1]) {
-          let maxYearBuiltFilter = MAX_YEAR_BUILT_FILTER;
-          // @ts-ignore
-          maxYearBuiltFilter[2] = currentFilterBuildDate[1]
-          allFeltFormattedFilters.push(maxYearBuiltFilter)
-        }
-
-        if (currentFilterLivingUnitsCategory.length) {
-          let livingUnitsCategoryFilter = LIVING_UNITS_CATEGORY_FILTER;
-          // @ts-ignore
-          livingUnitsCategoryFilter[2] = currentFilterLivingUnitsCategory
-          allFeltFormattedFilters.push(livingUnitsCategoryFilter)
-        }
-
-        if (currentLandUseZoningValues.length) {
-          let landUseZoningFilter = LUC_ZONING_FELT_FILTER_MAP.get(currentLandUseZoningFilter[0]);
-          if (landUseZoningFilter) {
-            // @ts-ignore
-            landUseZoningFilter[2] = currentLandUseZoningValues
-            allFeltFormattedFilters.push(landUseZoningFilter);
-          }
-        }
-
-        if (currentGeoFilteredValues.length) {
-          let currentGeoFilter = GEOGRAPHIC_FELT_FILTER_MAP.get(currentGeographicFilter[0])
-          if (currentGeoFilter) {
-            // @ts-ignore
-            currentGeoFilter[2] = currentGeoFilteredValues
-            allFeltFormattedFilters.push(currentGeoFilter);
-          }
-        }
-
-        const newFilters = filterUtils.andMany(allFeltFormattedFilters);
-
-        let layerToUpdate = THEME_TO_PARCEL_LAYER_MAP.get(currentTheme);
-
-        if (layerToUpdate) {
-          await felt.setLayerFilters({
-            layerId: layerToUpdate,
-            filters: newFilters
-          });
-        }
-      }
-    }
-
-    updateLayerFilter().catch(console.error);
-  }, [felt, currentFilters, currentFilterBuildDate, currentFilterLandUseCategory, currentGeoFilteredValues, currentFilterLivingUnitsCategory, currentLandUseZoningValues])
-
-  useEffect(() => {
     if (showAggregations && THEMES_WITHOUT_AGGREGATIONS.includes(currentTheme)) {
       return setShowAggregations(false);
     }
@@ -299,78 +212,6 @@ export default function Page() {
 
   async function handleThemeClick(theme: string) {
     setCurrentTheme(theme);
-  }
-
-  async function handleGeoFilterChange(value: string) {
-    // @ts-ignore
-    setCurrentGeographicFilter(value);
-    setCurrentGeoFilteredValues([]);
-  }
-
-  async function handleGeoFilterValueClick(value: Array<string>) {
-    // @ts-ignore
-    setCurrentGeoFilteredValues(value);
-  }
-
-  async function handleLuZFilterChange(value: string) {
-    // @ts-ignore
-    setcurrentLandUseZoningFilter(value);
-    setcurrentLandUseZoningValues([]);
-  }
-
-  async function handleLandUseZoningFilterClick(value: Array<string>) {
-    // @ts-ignore
-    setcurrentLandUseZoningValues(value);
-  }
-
-  async function handleYearBuiltInputChange(index: number, value: string) {
-    if (value.length !== 4) {
-      return;
-    }
-    const newValues = currentFilterBuildDate.map((val, i) => {
-      if (i === index) {
-        return parseInt(value, 10);
-      } else {
-        return val;
-      }
-    });
-    return setCurrentFilterBuildDate(newValues);
-  }
-
-  async function handleFilterClick(filter?: string) {
-    if (!filter) {
-      setCurrentFilters([]);
-      setCurrentFilterBuildDate(DEFAULT_BUILT_YEAR_FILTERS);
-      setCurrentFilterLivingUnitsCategory([]);
-      setCurrentFilterLandUseCategory([]);
-      setcurrentLandUseZoningFilter([]);
-      setCurrentGeographicFilter([]);
-      setCurrentGeoFilteredValues([]);
-      // @ts-ignore
-    } else if ((filter === MFH_HOMES.filter && currentFilters.includes(SFH_HOMES.filter)) || (filter === SFH_HOMES.filter && currentFilters.includes(MFH_HOMES.filter))) {
-      let newFilters: Array<any> = currentFilters.filter(item => item !== SFH_HOMES.filter && item !== MFH_HOMES.filter);
-      newFilters.push(SFH_MFH_HOMES.filter);
-      // @ts-ignore
-      setCurrentFilters(newFilters);
-      // @ts-ignore
-    } else if (filter === MFH_HOMES.filter && currentFilters.includes(SFH_MFH_HOMES.filter)) {
-      let newFilters: Array<any> = currentFilters.filter(item => item !== SFH_MFH_HOMES.filter);
-      newFilters.push(SFH_HOMES.filter);
-      // @ts-ignore
-      setCurrentFilters(newFilters);
-      // @ts-ignore
-    } else if (filter === SFH_HOMES.filter && currentFilters.includes(SFH_MFH_HOMES.filter)) {
-      let newFilters: Array<any> = currentFilters.filter(item => item !== SFH_MFH_HOMES.filter);
-      newFilters.push(MFH_HOMES.filter);
-      // @ts-ignore
-      setCurrentFilters(newFilters);
-      // @ts-ignore
-    } else if (!currentFilters.includes(filter)) {
-      // @ts-ignore
-      setCurrentFilters([...currentFilters, filter]);
-    } else {
-      setCurrentFilters(currentFilters.filter(x => x !== filter));
-    }
   }
 
   function handleResetTour() {
@@ -450,7 +291,7 @@ export default function Page() {
                   <>
                     <FilterSwitch showFilters={showFilters} onButtonClick={setShowFilters} />
                     <Button
-                      onClick={() => handleFilterClick()}
+                      onClick={() => setFilterKey(filterKey => filterKey + 1)}
                       variant="surface"
                       size="xs"
                     >
@@ -466,35 +307,7 @@ export default function Page() {
                 <HelpMenu onResetTour={handleResetTour} />
               </HStack>
             </Flex>
-            {showFilters &&
-              <Stack>
-                <Flex align="baseline" gap="4" wrap="wrap" paddingBottom="2">
-                  <FilterSelection
-                    currentFilters={currentFilters}
-                    onFilterClick={handleFilterClick} />
-                  <Flex gap="1">
-                    <DateRangeSlider
-                      value={currentFilterBuildDate}
-                      onDateSliderChange={setCurrentFilterBuildDate}
-                      onDateInputChange={handleYearBuiltInputChange}
-                    />
-                    <LivingUnitsCategorySelect
-                      value={currentFilterLivingUnitsCategory}
-                      onSelectChange={setCurrentFilterLivingUnitsCategory} />
-                    <GeographicFiltersSelect
-                      geoFilter={currentGeographicFilter}
-                      geoValues={currentGeoFilteredValues}
-                      onFilterChange={handleGeoFilterChange}
-                      onFilterValueChange={handleGeoFilterValueClick}
-                    />
-                    <LandUseCategorySelect
-                      landUseZonFilter={currentLandUseZoningFilter}
-                      luzValues={currentLandUseZoningValues}
-                      onFilterChange={handleLuZFilterChange}
-                      onFilterValueChange={handleLandUseZoningFilterClick} />
-                  </Flex>
-                </Flex>
-              </Stack>}
+            {showFilters && <FilterPane key={filterKey} felt={felt} currentTheme={currentTheme} />}
           </Stack>
         </Stack>
         <Box
---
+++ src/components/FilterPane.tsx
@@ -0,0 +1,212 @@
+import { useEffect, useState } from "react";
+import {
+    Flex,
+    Stack,
+} from "@chakra-ui/react";
+import {
+    FeltController,
+    Filters,
+} from "@feltmaps/js-sdk";
+
+import { FilterSelection } from "./FilterSelection";
+import { DateRangeSlider } from "./DateRangeSlider";
+import { LivingUnitsCategorySelect } from "./LivingUnitsCategorySelect";
+import { LandUseCategorySelect } from "./LandUseCategorySelect";
+import { GeographicFiltersSelect } from "./GeographicFiltersSelect";
+
+import {
+    DEFAULT_BUILT_YEAR_FILTERS,
+    FILTERS_TO_FELT_FILTER,
+    GEOGRAPHIC_FELT_FILTER_MAP,
+    LIVING_UNITS_CATEGORY_FILTER,
+    LUC_ZONING_FELT_FILTER_MAP,
+    MIN_YEAR_BUILT_FILTER,
+    MFH_HOMES,
+    MAX_YEAR_BUILT_FILTER,
+    SFH_HOMES,
+    SFH_MFH_HOMES,
+    THEME_TO_PARCEL_LAYER_MAP,
+} from "../constants";
+import { filterUtils } from "../utils";
+
+interface FilterPaneProps {
+    felt: FeltController | null;
+    currentTheme: string;
+}
+
+export const FilterPane = ({ felt, currentTheme }: FilterPaneProps) => {
+    const [currentFilters, setCurrentFilters] = useState([]);
+    const [currentFilterBuildDate, setCurrentFilterBuildDate] = useState(DEFAULT_BUILT_YEAR_FILTERS);
+    const [currentFilterLivingUnitsCategory, setCurrentFilterLivingUnitsCategory] = useState([]);
+    const [currentFilterLandUseCategory, setCurrentFilterLandUseCategory] = useState([]);
+    const [currentLandUseZoningFilter, setcurrentLandUseZoningFilter] = useState([]);
+    const [currentLandUseZoningValues, setcurrentLandUseZoningValues] = useState([]);
+    const [currentGeographicFilter, setCurrentGeographicFilter] = useState([]);
+    const [currentGeoFilteredValues, setCurrentGeoFilteredValues] = useState([]);
+
+    useEffect(() => {
+        const updateLayerFilter = async () => {
+            if (felt) {
+
+                const allLayerFilters = new Map(FILTERS_TO_FELT_FILTER);
+                const allFeltFormattedFilters: Filters[] = currentFilters.map((f) => allLayerFilters.get(f) || null);
+
+                if (currentFilterBuildDate[0] !== DEFAULT_BUILT_YEAR_FILTERS[0]) {
+                    let minYearBuiltFilter = MIN_YEAR_BUILT_FILTER;
+                    // @ts-ignore
+                    minYearBuiltFilter[2] = currentFilterBuildDate[0]
+                    allFeltFormattedFilters.push(minYearBuiltFilter)
+                }
+
+                if (currentFilterBuildDate[1] !== DEFAULT_BUILT_YEAR_FILTERS[1]) {
+                    let maxYearBuiltFilter = MAX_YEAR_BUILT_FILTER;
+                    // @ts-ignore
+                    maxYearBuiltFilter[2] = currentFilterBuildDate[1]
+                    allFeltFormattedFilters.push(maxYearBuiltFilter)
+                }
+
+                if (currentFilterLivingUnitsCategory.length) {
+                    let livingUnitsCategoryFilter = LIVING_UNITS_CATEGORY_FILTER;
+                    // @ts-ignore
+                    livingUnitsCategoryFilter[2] = currentFilterLivingUnitsCategory
+                    allFeltFormattedFilters.push(livingUnitsCategoryFilter)
+                }
+
+                if (currentLandUseZoningValues.length) {
+                    let landUseZoningFilter = LUC_ZONING_FELT_FILTER_MAP.get(currentLandUseZoningFilter[0]);
+                    if (landUseZoningFilter) {
+                        // @ts-ignore
+                        landUseZoningFilter[2] = currentLandUseZoningValues
+                        allFeltFormattedFilters.push(landUseZoningFilter);
+                    }
+                }
+
+                if (currentGeoFilteredValues.length) {
+                    let currentGeoFilter = GEOGRAPHIC_FELT_FILTER_MAP.get(currentGeographicFilter[0])
+                    if (currentGeoFilter) {
+                        // @ts-ignore
+                        currentGeoFilter[2] = currentGeoFilteredValues
+                        allFeltFormattedFilters.push(currentGeoFilter);
+                    }
+                }
+
+                const newFilters = filterUtils.andMany(allFeltFormattedFilters);
+
+                let layerToUpdate = THEME_TO_PARCEL_LAYER_MAP.get(currentTheme);
+
+                if (layerToUpdate) {
+                    await felt.setLayerFilters({
+                        layerId: layerToUpdate,
+                        filters: newFilters
+                    });
+                }
+            }
+        }
+
+        updateLayerFilter().catch(console.error);
+    }, [felt, currentFilters, currentFilterBuildDate, currentFilterLandUseCategory, currentLandUseZoningValues, currentGeoFilteredValues, currentFilterLivingUnitsCategory]);
+
+    async function handleGeoFilterChange(value: string) {
+        // @ts-ignore
+        setCurrentGeographicFilter(value);
+        setCurrentGeoFilteredValues([]);
+    }
+
+    async function handleGeoFilterValueClick(value: Array<string>) {
+        // @ts-ignore
+        setCurrentGeoFilteredValues(value);
+    }
+
+    async function handleLuZFilterChange(value: string) {
+        // @ts-ignore
+        setcurrentLandUseZoningFilter(value);
+        setcurrentLandUseZoningValues([]);
+    }
+
+    async function handleLandUseZoningFilterClick(value: Array<string>) {
+        // @ts-ignore
+        setcurrentLandUseZoningValues(value);
+    }
+
+    async function handleYearBuiltInputChange(index: number, value: string) {
+        if (value.length !== 4) {
+            return;
+        }
+        const newValues = currentFilterBuildDate.map((val, i) => {
+            if (i === index) {
+                return parseInt(value, 10);
+            } else {
+                return val;
+            }
+        });
+        return setCurrentFilterBuildDate(newValues);
+    }
+
+    async function handleFilterClick(filter?: string) {
+        if (!filter) {
+            setCurrentFilters([]);
+            setCurrentFilterBuildDate(DEFAULT_BUILT_YEAR_FILTERS);
+            setCurrentFilterLivingUnitsCategory([]);
+            setCurrentFilterLandUseCategory([]);
+            setcurrentLandUseZoningFilter([]);
+            setCurrentGeographicFilter([]);
+            setCurrentGeoFilteredValues([]);
+            // @ts-ignore
+        } else if ((filter === MFH_HOMES.filter && currentFilters.includes(SFH_HOMES.filter)) || (filter === SFH_HOMES.filter && currentFilters.includes(MFH_HOMES.filter))) {
+            let newFilters: Array<any> = currentFilters.filter(item => item !== SFH_HOMES.filter && item !== MFH_HOMES.filter);
+            newFilters.push(SFH_MFH_HOMES.filter);
+            // @ts-ignore
+            setCurrentFilters(newFilters);
+            // @ts-ignore
+        } else if (filter === MFH_HOMES.filter && currentFilters.includes(SFH_MFH_HOMES.filter)) {
+            let newFilters: Array<any> = currentFilters.filter(item => item !== SFH_MFH_HOMES.filter);
+            newFilters.push(SFH_HOMES.filter);
+            // @ts-ignore
+            setCurrentFilters(newFilters);
+            // @ts-ignore
+        } else if (filter === SFH_HOMES.filter && currentFilters.includes(SFH_MFH_HOMES.filter)) {
+            let newFilters: Array<any> = currentFilters.filter(item => item !== SFH_MFH_HOMES.filter);
+            newFilters.push(MFH_HOMES.filter);
+            // @ts-ignore
+            setCurrentFilters(newFilters);
+            // @ts-ignore
+        } else if (!currentFilters.includes(filter)) {
+            // @ts-ignore
+            setCurrentFilters([...currentFilters, filter]);
+        } else {
+            setCurrentFilters(currentFilters.filter(x => x !== filter));
+        }
+    }
+
+    return (
+        <Stack>
+            <Flex align="baseline" gap="4" wrap="wrap" paddingBottom="2">
+                <FilterSelection
+                    currentFilters={currentFilters}
+                    onFilterClick={handleFilterClick} />
+                <Flex gap="1">
+                    <DateRangeSlider
+                        value={currentFilterBuildDate}
+                        onDateSliderChange={setCurrentFilterBuildDate}
+                        onDateInputChange={handleYearBuiltInputChange}
+                    />
+                    <LivingUnitsCategorySelect
+                        value={currentFilterLivingUnitsCategory}
+                        onSelectChange={setCurrentFilterLivingUnitsCategory} />
+                    <GeographicFiltersSelect
+                        geoFilter={currentGeographicFilter}
+                        geoValues={currentGeoFilteredValues}
+                        onFilterChange={handleGeoFilterChange}
+                        onFilterValueChange={handleGeoFilterValueClick}
+                    />
+                    <LandUseCategorySelect
+                        landUseZonFilter={currentLandUseZoningFilter}
+                        luzValues={currentLandUseZoningValues}
+                        onFilterChange={handleLuZFilterChange}
+                        onFilterValueChange={handleLandUseZoningFilterClick} />
+                </Flex>
+            </Flex>
+        </Stack>
+    )
+
+}
